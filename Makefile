667f3b2e6ed3085b1a8f5b1b7d3f106ae4ec0344
Wolfgang Meier <womeier@posteo.de>
2023-04-10 21:40:55 +0200

    update wasm_to_coq to check module typing

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
-all:
+all: foo
+
+foo:
 	coqc test.v
 #	cat foo.ir
 #	echo "-----------------------------------------"
 	wasm2wat foo.wasm > foo.wat
 	nodejs foo.js
 
+foo_check:
+	@python3 wasm_to_coq.py foo.wasm
+	coqc foo_wasm.v
+
 sha: sha256.vo
 	ulimit -s unlimited && coqc test_sha.v
 	wasm2wat sha.wasm > sha.wat
 	python3 compare_output.py ./sha.js ./sha_output.txt
 
-sha256.vo: sha256.v
+%.vo: %.v
 	coqc $<
 
 clean:
--- wasm_to_coq.py
+++ wasm_to_coq.py
@@ -19,20 +19,40 @@ def b2s(b):
 
 
 preamble = """ 
-Require Import Byte.
-From parseque Require Import Parseque.
+Unset Universe Checking.
+
+From Coq Require Import String.
+From Wasm Require Import datatypes prettyprint.
+
+From CertiCoq Require Import LambdaANF.toplevel Common.Common Common.compM Common.Pipeline_utils.
+Require Import ExtLib.Structures.Monad.
+From MetaCoq.Template Require Import bytestring MCString.
+From Coq Require Import ZArith List.
+
+From CertiCoq Require Import LambdaANF.cps LambdaANF.cps_show CodegenWASM.wasm_map_util.
+Import MonadNotation.
+
+From Coq.Strings Require Import Byte.
+
 From Wasm Require Import binary_format_parser binary_format_printer
-                         datatypes_properties check_toks.
+                         datatypes_properties check_toks instantiation.
 """
 
 definition_bytelist = "Definition test_bytes : list Byte.byte := " + " :: ".join([b2s(x) for x in bytestring]) + " :: nil."
 definition_module = "Definition test_module : option module := run_parse_module test_bytes."
 compute = "Compute test_module."
+type_check = """Definition type_check_test_module :=
+    m <- test_module ;;
+    module_type_checker m.
+"""
+compute_type_check = "Compute type_check_test_module."
 
-file_name_out = file_name + ".v"
+file_name_out = file_name.replace(".", "_") + ".v"
 if os.path.isfile(file_name_out):
     os.remove(file_name_out)
 
+def debug_compute(text):
+    return f"Compute \"{text}\"%string."
 
 print(f"writing output to {file_name_out}")
 
@@ -40,4 +60,8 @@ with open(file_name_out, "a") as f:
     f.write(preamble + "\n\n")
     f.write(definition_bytelist + "\n\n")
     f.write(definition_module + "\n\n")
+    f.write(debug_compute("parsing module bytes") + "\n\n")
     f.write(compute + "\n\n")
+    f.write(type_check + "\n\n")
+    f.write(debug_compute("type checking") + "\n\n")
+    f.write(compute_type_check + "\n\n")
