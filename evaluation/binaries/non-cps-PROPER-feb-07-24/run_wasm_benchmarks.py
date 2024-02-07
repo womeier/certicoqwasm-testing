fc9666b6cadfa17e7d14106ff7d61d89a4a8c729
Wolfgang Meier <womeier@posteo.de>
2024-02-07 23:07:41 +0100

    add new wasm binaries

---
+++ evaluation/binaries/non-cps-PROPER-feb-07-24/run_wasm_benchmarks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+CWD = os.path.abspath(os.path.dirname(__file__))
+os.chdir(CWD)
+
+files = open("TESTS").read().strip().split("\n")
+
+ret_code = 0
+
+for f in files:
+    name = f"./CertiCoq.Benchmarks.tests.{f}.js"
+    assert os.path.isfile(name), f"didn't find js file {name}, did you run <make compilewasm>?"
+
+    print(f"\nrunning: {name}")
+    r = subprocess.run(["node", "--experimental-wasm-return_call", name])
+
+    if r.returncode != 0:
+        ret_code = r.returncode
+
+sys.exit(ret_code)
