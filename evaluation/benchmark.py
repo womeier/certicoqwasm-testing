f2f0a4bb812c9c01b2a8bdb561177446b04bf42f
Martin Karup Jensen <karup1337@gmail.com>
2024-04-05 13:02:30 +0200

    Add latex + org table printing to benchmark script

--- evaluation/benchmark.py
+++ evaluation/benchmark.py
@@ -6,6 +6,7 @@ import subprocess
 import json
 from tqdm import tqdm
 import time
+import pathlib
 
 CWD = os.path.abspath(os.path.dirname(__file__))
 os.chdir(CWD)
@@ -132,80 +133,173 @@ def single_run_wasmtime(folder, program, verbose):
     res = "{" + r.stdout.decode("ascii").split("{{")[1].split("}}")[0] + "}"
     return json.loads(res)
 
+def latex_table(tests, measure, results):
+    rows = [[binary_version] + ["N/A" if result.get(t) is None else f"{result[t][measure]}" for t in tests] for (binary_version, result) in results.items()]
+
+    latex_rows = [" & ".join(row) + "\\\\ \n" for row in rows]
+    latex_string = (("\\begin{table}\n")
+                    +("\\caption{placeholder caption}\n")
+                    +("\\label{tbl:placeholder-label}\n")
+                    +("\\centering\n")
+                    +("\\begin{tabular}{|" + "|".join(["l"] + ["r" for t in tests]) + "|}\n")
+                    +("\\hline\n")
+                    +(" & " + " & ".join(tests) + " \\\\")
+                    +("\\hline\n")
+                    +(("\\hline\n").join(latex_rows))
+                    +("\\hline\n")
+                    +("\\end{tabular}\n")
+                    +("\\end{table}\n")).replace("_", "\\_")
+
+    print(latex_string)
+
+
+
+def org_table(tests, measure, results):
+    rows = [[binary_version] + ["N/A" if result.get(t) is None else f"{result[t][measure]}" for t in tests] for (binary_version, result) in results.items()]
+
+    org_rows = ["| " + " | ".join(row) + " |\n" for row in rows]
+
+    org_string = (("|---|---" + "".join(["|---" for t in tests]) + "|\n")
+                  +("|   | " + " | ".join(tests) + " |\n")
+                  +("|---|---" + "".join(["|---" for t in tests]) + "|\n")
+                  +(("|---|---" + "".join(["|---" for t in tests]) + "|\n").join(org_rows))
+                  +("|---|---" + "".join(["|---" for t in tests]) + "|\n"))
+
+    print(org_string)
 
 @click.command()
 @click.option("--engine", type=str, help="Wasm engine", default="node")
 @click.option("--runs", type=int, help="Number of runs", default=10)
 @click.option("--memory_usage", is_flag=True, help="Print lin.mem. used", default=False)
 @click.option("--binary_size", is_flag=True, help="Print binary size", default=False)
-@click.option("--folder", type=str, help="Folder to Wasm binaries", required=True)
+@click.option("--folder", type=str, help="Folder to Wasm binaries", multiple=True, required=True)
 @click.option("--optimize_flag", type=str, help="Binaryen optimizations flag")
 @click.option("--verbose", is_flag=True, help="Print debug information", default=False)
-def measure(engine, runs, memory_usage, binary_size, folder, verbose, optimize_flag):
+@click.option("--tests", type=str, help="Path to file containing tests")
+@click.option("--print_latex_table", is_flag=True, help="Print results as latex table", default=False)
+@click.option("--print_org_table", is_flag=True, help="Print results as org mode table", default=False)
+def measure(engine, runs, memory_usage, binary_size, folder, verbose, optimize_flag, tests, print_latex_table, print_org_table):
     assert (
         engine == "wasmtime" or engine == "node"
     ), "Expected wasmtime or node runtime."
     assert runs > 0, "Expected at least one run."
 
-    description = get_info(folder.strip())
-    if optimize_flag is not None:
-        description = description + f" ({optimize_flag})"
-    engine_version = get_engine_version(engine)
-    print(f"Running {description}, avg. of {runs} runs in {engine_version}.")
+    tests_to_run = open(tests).read().strip().split("\n") if tests is not None else []
 
-    programs = open(f"{folder}/TESTS").read().strip().split("\n")
-    for program in programs:
-        path = f"{folder}/CertiCoq.Benchmarks.tests.{program}.wasm"
+    collected_tests = []
+
+    all_results = dict()
+
+    for f in folder:
+        f_name = pathlib.PurePath(f).name
+
+        description = get_info(f.strip())
         if optimize_flag is not None:
-            program = program_opt_name(program, optimize_flag)
-            path = f"{folder}/CertiCoq.Benchmarks.tests.{program}.wasm"
+            description = description + f" ({optimize_flag})"
+        engine_version = get_engine_version(engine)
+        print(f"Running {description}, avg. of {runs} runs in {engine_version}.")
+
+        programs = open(f"{f}/TESTS").read().strip().split("\n") if tests is None else tests_to_run
+        folder_results = dict()
+        for program in programs:
+
+            test = program
+
+            if program not in collected_tests:
+                collected_tests.append(program)
+
+            path = f"{f}/CertiCoq.Benchmarks.tests.{program}.wasm"
+
             if not os.path.exists(path):
-                print("Didn't find optimized binaries.")
-                create_optimized_programs(folder, flag=optimize_flag)
-                print("Done. Please run again.")
-                exit()
+                continue
 
-        values = []
-        for run in range(runs):
-            res = None
-            if engine == "node":
-                res = single_run_node(folder, program, verbose)
+            if optimize_flag is not None:
+                program = program_opt_name(program, optimize_flag)
+                path = f"{f}/CertiCoq.Benchmarks.tests.{program}.wasm"
+                if not os.path.exists(path):
 
-            if engine == "wasmtime":
-                res = single_run_wasmtime(folder, program, verbose)
+                    print(f"Didn't find optimized binaries: {program}")
+                    # print("Didn't find optimized binaries.")
+                    create_optimized_programs(f, flag=optimize_flag)
+                    print("Done. Please run again.")
+                    exit()
 
-            assert res is not None, "No value returned."
-            values.append(res)
+            values = []
+            for run in range(runs):
+                res = None
+                if engine == "node":
+                    res = single_run_node(f, program, verbose)
 
-        result = dict()
-        for meas in measurements:
-            result[meas] = list()
+                if engine == "wasmtime":
+                    res = single_run_wasmtime(f, program, verbose)
 
-        for val in values:
+                assert res is not None, "No value returned."
+                values.append(res)
+
+            result = dict()
             for meas in measurements:
-                result[meas].append(int(val[meas]))
+                result[meas] = list()
 
-        time_startup = round(sum(result["time_startup"]) / len(result["time_startup"]))
-        time_main = round(sum(result["time_main"]) / len(result["time_main"]))
-        time_pp = round(sum(result["time_pp"]) / len(result["time_pp"]))
+            for val in values:
+                for meas in measurements:
+                    result[meas].append(int(val[meas]))
 
-        memory_in_kb = int(result["bytes_used"][0] / 1000) if runs > 0 else "N/A"
-        binary_size_in_kb = int(os.stat(path).st_size / 1000)
+            time_startup = round(sum(result["time_startup"]) / len(result["time_startup"]))
+            time_main = round(sum(result["time_main"]) / len(result["time_main"]))
+            time_pp = round(sum(result["time_pp"]) / len(result["time_pp"]))
 
-        # count spaces instead of using \t
-        max_program_len = max(map(len, programs))
-        program_orig_len = (
-            len(program.split("-opt_")[0]) if "-opt_" in program else len(program)
-        )
-        program_pp = (max_program_len - program_orig_len) * " " + program
+            memory_in_kb = int(result["bytes_used"][0] / 1000) if runs > 0 else "N/A"
+            binary_size_in_kb = int(os.stat(path).st_size / 1000)
 
-        print(
-            f"{program_pp} : "
-            f"startup: {time_startup:>4}, main: {time_main:>3}, pp: {time_pp:>2}"
-            f", sum: {time_startup+time_main+time_pp:>4}"
-            + (f", memory used: {memory_in_kb} KB" if memory_usage else "")
-            + (f", bin size: {binary_size_in_kb:>4} KB" if binary_size else "")
-        )
+            folder_results[test] = {"time_startup": time_startup, "time_main": time_main, "time_pp": time_pp, "bytes_used": memory_in_kb, "binary_size_in_kb": binary_size_in_kb}
+
+            # count spaces instead of using \t
+            max_program_len = max(map(len, programs))
+            program_orig_len = (
+                len(program.split("-opt_")[0]) if "-opt_" in program else len(program)
+            )
+            program_pp = (max_program_len - program_orig_len) * " " + program
+
+            print(
+                f"{program_pp} : "
+                f"startup: {time_startup:>4}, main: {time_main:>3}, pp: {time_pp:>2}"
+                f", sum: {time_startup+time_main+time_pp:>4}"
+                + (f", memory used: {memory_in_kb} KB" if memory_usage else "")
+                + (f", bin size: {binary_size_in_kb:>4} KB" if binary_size else "")
+            )
+
+        all_results[f_name] = folder_results
+
+    if print_latex_table:
+        print("\nPrinting LaTeX tables:\n")
+
+        for meas in measurements:
+
+            if not memory_usage and meas == "bytes_used":
+                continue
+
+            print(f"\nPrinting LaTeX table for {meas}:")
+            latex_table(collected_tests, meas, all_results)
+
+        if binary_size:
+            print(f"\nPrinting LaTeX table for binary size:")
+            latex_table(collected_tests, "binary_size_in_kb", all_results)
+
+    if print_org_table:
+        print("\nPrinting org tables:\n")
+
+        for meas in measurements:
+            if not memory_usage and meas == "bytes_used":
+                continue
+
+            print(f"\nPrinting org table for {meas}:")
+            org_table(collected_tests, meas, all_results)
+
+        if binary_size:
+            print(f"\nPrinting org table for binary size:")
+            org_table(collected_tests, "binary_size_in_kb", all_results)
+
+    print("")
 
 
 if __name__ == "__main__":
