49acd1fbf08a40c475adc06cec7a98a78f86f2e6
Wolfgang Meier <womeier@posteo.de>
2024-04-26 11:32:00 +0200

    basic example for wasmgc/ref calls

---
+++ wasm_examples/add.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const bytes = fs.readFileSync(__dirname + '/add.wasm');
+const val_1 = 41;
+const val_2 = 1;
+
+
+let importObject = {
+    env: {
+        log_i32: (value) => { console.log ("logging i32: ", value) },
+        log_i64: (value) => { console.log ("logging i64: ", value) },
+    },
+/*    env: {
+        import_i32: 5_000_000_000, // _ is ignored in numbers in JS and WAT
+        import_f32: 123.0123456789,
+        import_f64: 123.0123456789,
+    } */
+};
+
+(async () => {
+    const obj = await WebAssembly.instantiate(
+        new Uint8Array (bytes), importObject
+    );
+
+    let res = obj.instance.exports.add(val_1, val_2);
+    console.log(`===> ${res}`);
+})();
