d8a376e71dcb09add663196acb7d93201c1348d7
Wolfgang Meier <womeier@posteo.de>
2024-05-16 22:21:37 +0200

    simplify

--- evaluation/run-node.js
+++ evaluation/run-node.js
@@ -84,51 +84,49 @@ let importObject = {
         let out_of_mem = obj.instance.exports.result_out_of_mem;
         let bytes_used = obj.instance.exports.bytes_used;
         if (bytes_used == undefined) {
-            // WasmGC
+            // constructor repr: WasmGC
             bytes_used = -1; // unknown mem usage: TODO ask v8
-            out_of_mem = 0; // success
-        } else {
-            bytes_used = bytes_used.value;
-            out_of_mem = out_of_mem.value;
+            console.log(`Benchmark ${path}: {{"time_startup": "${time_startup}", "time_main": "${time_main}", "time_pp": "0", "bytes_used": "${bytes_used}", "program": "${program}"}} ms, bytes.`);
+            process.exit(0);
         }
-        let time_pp = undefined;
+        // constructor repr: linear memory
+        bytes_used = bytes_used.value;
+        out_of_mem = out_of_mem.value;
 
         if (out_of_mem == 1) {
             console.log("Ran out of memory.");
             console.log(`Benchmark ${path}: {{"time_startup": "${time_startup}", "time_main": "${time_main}", "program": "${program}"}} (in ms)`);
             process.exit(1);
-        } else {
-            const res_value = obj.instance.exports.result.value;
-            process.stdout.write("====> ");
+        }
 
-            // with the new constructor representation, modules don't contain a generated pp function anymore
-            let fn_pp = obj.instance.exports.pretty_print_constructor;
+        // successful result
+        const res_value = obj.instance.exports.result.value;
+        process.stdout.write("====> ");
+
+        let time_pp = undefined;
+        let fn_pp = obj.instance.exports.pretty_print_constructor;
+        if (fn_pp == undefined) {
+            // new constructor representation (like C backend), doesn't contain automatically generated PP fn
+            fn_pp = pp_map[program.split("-opt")[0]];
             if (fn_pp == undefined) {
-                fn_pp = pp_map[program.split("-opt")[0]];
-                if (fn_pp == undefined) {
-                    console.log(`Please specify pp functions for ${program} in run-node.js.`);
-                    process.exit(1);
-                }
-
-                const memory = obj.instance.exports.memory;
-                if (memory == undefined) {
-                    console.log("WasmGC, pp not supported.");
-                    time_pp = 0;
-                } else {
-                    const dataView = new DataView(memory.buffer);
-                    const res_value = obj.instance.exports.result.value;
-
-                    const start_pp = Date.now();
-                    fn_pp(res_value, dataView);
-                    const stop_pp = Date.now();
-                    time_pp = stop_pp - start_pp;
-                }
-            } else {
-                const start_pp = Date.now();
-                obj.instance.exports.pretty_print_constructor(res_value);
-                const stop_pp = Date.now();
-                time_pp = stop_pp - start_pp;
+                console.log(`Please specify pp functions for ${program} in run-node.js.`);
+                process.exit(1);
             }
+
+            const memory = obj.instance.exports.memory;
+            const dataView = new DataView(memory.buffer);
+            const res_value = obj.instance.exports.result.value;
+
+            const start_pp = Date.now();
+            fn_pp(res_value, dataView);
+            const stop_pp = Date.now();
+            time_pp = stop_pp - start_pp;
+        } else {
+            // old constructor representation (unlike C backend), contained automatically generated PP fn
+            const start_pp = Date.now();
+            obj.instance.exports.pretty_print_constructor(res_value);
+            const stop_pp = Date.now();
+            time_pp = stop_pp - start_pp;
         }
         console.log(`\nBenchmark ${path}: {{"time_startup": "${time_startup}", "time_main": "${time_main}", "time_pp": "${time_pp}", "bytes_used": "${bytes_used}", "program": "${program}"}} ms, bytes.`);
     } catch (error) {
