e810fbff589a575c8df7f5a302cbfb0293309946
Martin Karup Jensen <karup1337@gmail.com>
2024-07-14 16:36:55 +0200

    Binaries for malfunction extraction (via wasm_of_ocaml)
    Note: The "O2" in the binaries_O2 refers to optimizations applied when invoking the malfunction
    compiler (I.e. malfunction cmo -O2 ...)

---
+++ evaluation/malfunction_to_wasm/benchmark.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+import os
+import click
+import subprocess
+import json
+import pathlib
+from tqdm import tqdm
+
+CWD = os.path.abspath(os.path.dirname(__file__))
+os.chdir(CWD)
+
+# NODE = "/usr/bin/node"
+NODE = "node"
+measurements = ["time_startup", "time_main"]
+
+programs = [
+    "demo1",
+    "demo2",
+    "list_sum",
+    "vs_easy",
+    "vs_hard",
+    "binom",
+    "sha_fast",
+    "color",
+    "sm_gauss_nat",
+    "sm_gauss_N",
+    "even_10000",
+    "ack_3_9",
+    # "bernstein_yang"
+]
+
+
+def get_engine_version(engine):
+    if engine == "node":
+        r = subprocess.run([NODE, "--version"], capture_output=True)
+        return f"node ({r.stdout.decode('ascii').strip()})"
+    else:
+        print(f"Engine {engine} not found.")
+        exit(1)
+
+
+def single_run_node(folder, program, verbose):
+    r = subprocess.run(
+        [
+            NODE,
+            "--stack-size=1000000",
+            "./run-node.js",
+            folder,
+            program,
+        ],
+        capture_output=True,
+    )
+
+    wasm_path = f"{folder}/{program}.wasm"
+    # assert (
+    #     r.returncode == 0
+    # ), f"Running {wasm_path} returned non-0 returncode, stderr: {r.stderr}"
+
+    if verbose:
+        print("STDOUT: " + r.stdout.decode("ascii"))
+        print("STDERR: " + r.stdout.decode("ascii"))
+
+    res = "{" + r.stdout.decode("ascii").split("{{")[1].split("}}")[0] + "}"
+    return json.loads(res)
+
+def program_opt_name(program, flags):
+    flags = map(lambda f: f.replace("-", ""), flags)
+    return f"{program}-opt_{'-'.join(flags)}"
+
+
+def create_optimized_programs(folder, flags):
+    print(f"Creating programs optimized with wasm-opt {' '.join(flags)} in {folder}.")
+    for program in tqdm(programs):
+        program_opt = program_opt_name(program, flags)
+        path_orig = os.path.join(folder, f"{program}.wasm")
+        path_opt = os.path.join(folder, f"{program_opt}.wasm")
+        if not os.path.exists(path_opt):
+            subprocess.run(
+                [
+                    "wasm-opt",
+                    *flags,
+                    "--enable-tail-call",
+                    "--enable-reference-types",
+                    "--enable-gc",
+                    "--enable-mutable-globals",
+                    "--enable-bulk-memory",
+                    "--enable-exception-handling",
+                    "--enable-multivalue",
+                    "--enable-strings",
+                    "--enable-nontrapping-float-to-int",
+                    path_orig,
+                    "--output",
+                    path_opt,
+                ]
+            )
+
+@click.command()
+@click.option("--runs", type=int, help="Number of runs", default=10)
+@click.option("--binary-size", is_flag=True, help="Print binary size", default=False)
+@click.option("--folder", type=str, help="Folder to Wasm binaries", required=True)
+@click.option("--wasm-opt", type=str, help="Wasm-opt optimizations flag", multiple=True)
+@click.option("--verbose", is_flag=True, help="Print debug information", default=False)
+def measure(runs, binary_size, folder, wasm_opt, verbose):
+    assert runs > 0, "Expected at least one run."
+
+    f_name = pathlib.PurePath(folder).name
+    engine_version = get_engine_version("node")
+    print(f"Running {f_name}, avg. of {runs} runs in {engine_version}.")
+
+    for program in programs:
+        program_name_orig = program
+        path = f"{folder}/{program}.wasm"
+        if not os.path.exists(path):
+            continue
+
+        if wasm_opt:
+            program = program_opt_name(program, wasm_opt)
+            path = f"{folder}/{program}.wasm"
+            if not os.path.exists(path):
+                print(f"Didn't find optimized binary: {path}")
+                create_optimized_programs(folder, wasm_opt)
+
+        values = []
+        for run in range(runs):
+            res = single_run_node(folder, program, verbose)
+
+            assert res is not None, "No value returned."
+            values.append(res)
+
+        result = dict()
+        for meas in measurements:
+            result[meas] = list()
+
+        for val in values:
+            for meas in measurements:
+                result[meas].append(int(val[meas]))
+
+        time_startup = round(sum(result["time_startup"]) / len(result["time_startup"]))
+        time_main = round(sum(result["time_main"]) / len(result["time_main"]))
+        # time_pp = round(sum(result["time_pp"]) / len(result["time_pp"]))
+
+        binary_size_in_kb = int(os.stat(path).st_size / 1000)
+
+        # count spaces instead of using \t
+        max_program_len = max(map(len, programs))
+        program_orig_len = (
+            len(program.split("-opt_")[0]) if "-opt_" in program else len(program)
+        )
+        program_pp = (max_program_len - program_orig_len) * " " + program
+
+        print(
+            f"{program_pp} : "
+            f"startup: {time_startup:>2}, main: {time_main:>3}"
+            f", sum: {time_startup+time_main:>4}"
+            + (f", bin size: {binary_size_in_kb:>4} KB" if binary_size else "")
+        )
+
+
+if __name__ == "__main__":
+    measure()
---
+++ evaluation/malfunction_to_wasm/run-node.js
@@ -0,0 +1,532 @@
+import {join, dirname} from "path";
+import {readFile} from "fs/promises";
+import {spawnSync} from "child_process";
+import * as fs1 from "fs";
+
+var args = process.argv.slice(2);
+if (args.length != 2) {
+    console.log("Expected two args: 0: path to folder containing wasm file to run, 1: program.");
+    console.log("e.g.: $ node run-node.js ./binaries/naive vs_easy");
+    process.exit(1);
+}
+let path = args[0];
+if (path.charAt(path.length - 1) != "/") { path = path + "/" }
+
+let program = args[1];
+
+(function(Object){
+   typeof globalThis !== "object"
+   &&
+    (this
+      ? get()
+      : (Object.defineProperty
+         (Object.prototype, "_T_", {configurable: true, get: get}),
+        _T_));
+   function get(){
+    var global = this || self;
+    global.globalThis = global;
+    delete Object.prototype._T_;
+   }
+  }
+  (Object));
+((js)=>
+      async args=>{
+       "use strict";
+       let {src: src, generated: generated} = args;
+	  function loadRelative(src){
+	      const f = `${path}${program}.wasm`;
+	   return readFile(f);
+       }
+       function fetchRelative(src){
+        const
+         base = globalThis?.document?.currentScript?.src,
+         url = base ? new URL(src, base) : src;
+        return fetch(url);
+       }
+	  const isNode = globalThis?.process?.versions?.node;
+        const code = isNode ? loadRelative(src) : fetchRelative(src);
+       let
+        math =
+          {cos: Math.cos,
+           sin: Math.sin,
+           tan: Math.tan,
+           acos: Math.acos,
+           asin: Math.asin,
+           atan: Math.atan,
+           cosh: Math.cosh,
+           sinh: Math.sinh,
+           tanh: Math.tanh,
+           acosh: Math.acosh,
+           asinh: Math.asinh,
+           atanh: Math.atanh,
+           cbrt: Math.cbrt,
+           exp: Math.exp,
+           expm1: Math.expm1,
+           log: Math.log,
+           log1p: Math.log1p,
+           log2: Math.log2,
+           log10: Math.log10,
+           atan2: Math.atan2,
+           hypot: Math.hypot,
+           pow: Math.pow,
+           fmod: (x, y)=>x % y},
+        typed_arrays =
+          [Float32Array,
+           Float64Array,
+           Int8Array,
+           Uint8Array,
+           Int16Array,
+           Uint16Array,
+           Int32Array,
+           Int32Array,
+           Int32Array,
+           Int32Array,
+           Float32Array,
+           Float64Array,
+           Uint8Array,
+           Uint8ClampedArray];
+	  const fs = isNode && fs1; 
+       let
+        fs_cst = fs?.constants,
+        open_flags =
+          fs
+           ? [fs_cst.RDONLY,
+             fs_cst.O_WRONLY,
+             fs_cst.O_APPEND,
+             fs_cst.O_CREAT,
+             fs_cst.O_TRUNC,
+             fs_cst.O_EXCL,
+             fs_cst.O_NONBLOCK]
+           : [];
+       var
+        out_channels =
+          {map: new WeakMap(),
+           set: new Set(),
+           finalization:
+           new FinalizationRegistry(ref=>out_channels.set.delete(ref))};
+       function register_channel(ch){
+        const ref = new WeakRef(ch);
+        out_channels.map.set(ch, ref);
+        out_channels.set.add(ref);
+        out_channels.finalization.register(ch, ref, ch);
+       }
+       function unregister_channel(ch){
+        const ref = out_channels.map.get(ch);
+        if(ref){
+         out_channels.map.delete(ch);
+         out_channels.set.delete(ref);
+         out_channels.finalization.unregister(ch);
+        }
+       }
+       function channel_list(){
+        return [...out_channels.set].map(ref=>ref.deref()).filter(ch=>ch);
+       }
+       var start_fiber;
+       function wrap_fun(t, f, a){
+        try{return new WebAssembly.Function(t, f, a);}catch(e){return f;}
+       }
+       const
+        decoder = new TextDecoder("utf-8", {ignoreBOM: 1}),
+        encoder = new TextEncoder;
+       function hash_int(h, d){
+        d = Math.imul(d, 0xcc9e2d51 | 0);
+        d = d << 15 | d >>> 17;
+        d = Math.imul(d, 0x1b873593);
+        h ^= d;
+        h = h << 13 | h >>> 19;
+        return (h + (h << 2) | 0) + (0xe6546b64 | 0) | 0;
+       }
+       function hash_string(h, s){
+        for(var i = 0; i < s.length; i++) h = hash_int(h, s.charCodeAt(i));
+        return h ^ s.length;
+       }
+       let
+        bindings =
+          {jstag:
+           WebAssembly.JSTag
+           || new WebAssembly.Tag({parameters: ["externref"], results: []}),
+           identity: x=>x,
+           from_bool: x=>! ! x,
+           get: (x, y)=>x[y],
+           set: (x, y, z)=>x[y] = z,
+           delete: (x, y)=>delete x[y],
+           instanceof: (x, y)=>x instanceof y,
+           typeof: x=>typeof x,
+           equals: (x, y)=>x == y,
+           strict_equals: (x, y)=>x === y,
+           fun_call: (f, o, args)=>f.apply(o, args),
+           meth_call: (o, f, args)=>o[f].apply(o, args),
+           new_array: n=>new Array(n),
+           new_obj: ()=>({}),
+           new: (c, args)=>new c(...args),
+           global_this: globalThis,
+           iter_props:
+           (o, f)=>{for(var nm in o) if(o.hasOwnProperty(nm)) f(nm);},
+           array_length: a=>a.length,
+           array_get: (a, i)=>a[i],
+           array_set: (a, i, v)=>a[i] = v,
+           read_string: l=>decoder.decode(new Uint8Array(buffer, 0, l)),
+           read_string_stream:
+           (l, stream)=>
+              decoder.decode(new Uint8Array(buffer, 0, l), {stream: stream}),
+           append_string: (s1, s2)=>s1 + s2,
+           write_string:
+           s=>{
+            var start = 0, len = s.length;
+            while(1){
+             let
+              {read: read, written: written} =
+                encoder.encodeInto(s.slice(start), out_buffer);
+             len -= read;
+             if(! len) return written;
+             caml_extract_string(written);
+             start += read;
+            }},
+           ta_create: (k, sz)=>new typed_arrays[k](sz),
+           ta_normalize:
+           a=>
+              a instanceof Uint32Array
+               ? new Int32Array(a.buffer, a.byteOffset, a.length)
+               : a,
+           ta_kind: a=>typed_arrays.findIndex(c=>a instanceof c),
+           ta_length: a=>a.length,
+           ta_get_f64: (a, i)=>a[i],
+           ta_get_f32: (a, i)=>a[i],
+           ta_get_i32: (a, i)=>a[i],
+           ta_get_i16: (a, i)=>a[i],
+           ta_get_ui16: (a, i)=>a[i],
+           ta_get_i8: (a, i)=>a[i],
+           ta_get_ui8: (a, i)=>a[i],
+           ta_set_f64: (a, i, v)=>a[i] = v,
+           ta_set_f32: (a, i, v)=>a[i] = v,
+           ta_set_i32: (a, i, v)=>a[i] = v,
+           ta_set_i16: (a, i, v)=>a[i] = v,
+           ta_set_ui16: (a, i, v)=>a[i] = v,
+           ta_set_i8: (a, i, v)=>a[i] = v,
+           ta_set_ui8: (a, i, v)=>a[i] = v,
+           ta_fill: (a, v)=>a.fill(v),
+           ta_blit: (s, d)=>d.set(s),
+           ta_subarray: (a, i, j)=>a.subarray(i, j),
+           ta_set: (a, b, i)=>a.set(b, i),
+           ta_new: len=>new Uint8Array(len),
+           ta_copy: (ta, t, s, n)=>ta.copyWithin(t, s, n),
+           ta_bytes:
+           a=>
+              new
+               Uint8Array
+               (a.buffer, a.byteOffset, a.length * a.BYTES_PER_ELEMENT),
+           wrap_callback:
+           f=>
+              function(){
+               var n = arguments.length;
+               if(n > 0){
+                var args = new Array(n);
+                for(var i = 0; i < n; i++) args[i] = arguments[i];
+               }
+               else
+                args = [undefined];
+               return caml_callback(f, args.length, args, 1);
+              },
+           wrap_callback_args:
+           f=>
+              function(){
+               var n = arguments.length, args = new Array(n);
+               for(var i = 0; i < n; i++) args[i] = arguments[i];
+               return caml_callback(f, 1, [args], 0);
+              },
+           wrap_callback_strict:
+           (arity, f)=>
+              function(){
+               var
+                n = arguments.length,
+                args = new Array(arity),
+                len = Math.min(arguments.length, arity);
+               for(var i = 0; i < len; i++) args[i] = arguments[i];
+               return caml_callback(f, arity, args, 0);
+              },
+           wrap_callback_unsafe:
+           f=>
+              function(){
+               var n = arguments.length, args = new Array(n);
+               for(var i = 0; i < n; i++) args[i] = arguments[i];
+               return caml_callback(f, args.length, args, 2);
+              },
+           wrap_meth_callback:
+           f=>
+              function(){
+               var n = arguments.length, args = new Array(n + 1);
+               args[0] = this;
+               for(var i = 0; i < n; i++) args[i + 1] = arguments[i];
+               return caml_callback(f, args.length, args, 1);
+              },
+           wrap_meth_callback_args:
+           f=>
+              function(){
+               var n = arguments.length, args = new Array(n);
+               for(var i = 0; i < n; i++) args[i] = arguments[i];
+               return caml_callback(f, 2, [this, args], 0);
+              },
+           wrap_meth_callback_strict:
+           (arity, f)=>
+              function(){
+               var
+                args = new Array(arity + 1),
+                len = Math.min(arguments.length, arity);
+               args[0] = this;
+               for(var i = 0; i < len; i++) args[i + 1] = arguments[i];
+               return caml_callback(f, args.length, args, 0);
+              },
+           wrap_meth_callback_unsafe:
+           f=>
+              function(){
+               var n = arguments.length, args = new Array(n + 1);
+               args[0] = this;
+               for(var i = 0; i < n; i++) args[i + 1] = arguments[i];
+               return caml_callback(f, args.length, args, 2);
+              },
+           wrap_fun_arguments: f=>function(){return f(arguments);},
+           format_float:
+           (prec, conversion, pad, x)=>{
+            function toFixed(x, dp){
+             if(Math.abs(x) < 1.0)
+              return x.toFixed(dp);
+             else{
+              var e = parseInt(x.toString().split("+")[1]);
+              if(e > 20){
+               e -= 20;
+               x /= Math.pow(10, e);
+               x += new Array(e + 1).join("0");
+               if(dp > 0) x = x + "." + new Array(dp + 1).join("0");
+               return x;
+              }
+              else
+               return x.toFixed(dp);
+             }
+            }
+            switch(conversion){
+              case 0:
+               var s = x.toExponential(prec), i = s.length;
+               if(s.charAt(i - 3) == "e")
+                s = s.slice(0, i - 1) + "0" + s.slice(i - 1);
+               break;
+              case 1:
+               s = toFixed(x, prec); break;
+              case 2:
+               prec = prec ? prec : 1;
+               s = x.toExponential(prec - 1);
+               var j = s.indexOf("e"), exp = + s.slice(j + 1);
+               if(exp < - 4 || x >= 1e21 || x.toFixed(0).length > prec){
+                var i = j - 1;
+                while(s.charAt(i) == "0") i--;
+                if(s.charAt(i) == ".") i--;
+                s = s.slice(0, i + 1) + s.slice(j);
+                i = s.length;
+                if(s.charAt(i - 3) == "e")
+                 s = s.slice(0, i - 1) + "0" + s.slice(i - 1);
+                break;
+               }
+               else{
+                var p = prec;
+                if(exp < 0){
+                 p -= exp + 1;
+                 s = x.toFixed(p);
+                }
+                else
+                 while(s = x.toFixed(p), s.length > prec + 1) p--;
+                if(p){
+                 var i = s.length - 1;
+                 while(s.charAt(i) == "0") i--;
+                 if(s.charAt(i) == ".") i--;
+                 s = s.slice(0, i + 1);
+                }
+               }
+               break;
+            }
+            return pad ? " " + s : s;},
+           gettimeofday: ()=>new Date().getTime() / 1000,
+           gmtime:
+           t=>{
+            var
+             d = new Date(t * 1000),
+             d_num = d.getTime(),
+             januaryfirst =
+               new Date(Date.UTC(d.getUTCFullYear(), 0, 1)).getTime(),
+             doy = Math.floor((d_num - januaryfirst) / 86400000);
+            return caml_alloc_tm
+                    (d.getUTCSeconds(),
+                     d.getUTCMinutes(),
+                     d.getUTCHours(),
+                     d.getUTCDate(),
+                     d.getUTCMonth(),
+                     d.getUTCFullYear() - 1900,
+                     d.getUTCDay(),
+                     doy,
+                     false);},
+           localtime:
+           t=>{
+            var
+             d = new Date(t * 1000),
+             d_num = d.getTime(),
+             januaryfirst = new Date(d.getFullYear(), 0, 1).getTime(),
+             doy = Math.floor((d_num - januaryfirst) / 86400000),
+             jan = new Date(d.getFullYear(), 0, 1),
+             jul = new Date(d.getFullYear(), 6, 1),
+             stdTimezoneOffset =
+               Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+            return caml_alloc_tm
+                    (d.getSeconds(),
+                     d.getMinutes(),
+                     d.getHours(),
+                     d.getDate(),
+                     d.getMonth(),
+                     d.getFullYear() - 1900,
+                     d.getDay(),
+                     doy,
+                     d.getTimezoneOffset() < stdTimezoneOffset);},
+           mktime:
+           (year, month, day, h, m, s)=>
+              new Date(year, month, day, h, m, s).getTime(),
+           random_seed: ()=>crypto.getRandomValues(new Int32Array(12)),
+           open:
+           (p, flags, perm)=>
+              fs.openSync
+               (p,
+                open_flags.reduce((f, v, i)=>flags & 1 << i ? f | v : f, 0),
+                perm),
+           close: fd=>fs.closeSync(fd),
+           write:
+           (fd, b, o, l, p)=>
+              fs
+               ? fs.writeSync(fd, b, o, l, p == null ? p : Number(p))
+               : (console
+                   [fd == 2 ? "error" : "log"]
+                  (typeof b == "string"
+                    ? b
+                    : decoder.decode(b.slice(o, o + l))),
+                 l),
+           read: (fd, b, o, l, p)=>fs.readSync(fd, b, o, l, p),
+           file_size: fd=>fs.fstatSync(fd, {bigint: true}).size,
+           register_channel: register_channel,
+           unregister_channel: unregister_channel,
+           channel_list: channel_list,
+           exit: n=>isNode && process.exit(n),
+           argv: ()=>isNode ? process.argv.slice(1) : ["a.out"],
+           getenv: n=>isNode ? process.env[n] : null,
+           system:
+           c=>{
+            var
+             res =
+               spawnSync
+                (c, {shell: true, stdio: "inherit"});
+            if(res.error) throw res.error;
+            return res.signal ? 255 : res.status;},
+           time: ()=>performance.now(),
+           getcwd: ()=>isNode ? process.cwd() : "/static",
+           chdir: x=>process.chdir(x),
+           mkdir: (p, m)=>fs.mkdirSync(p, m),
+           unlink: p=>fs.unlinkSync(p),
+           readdir: p=>fs.readdirSync(p),
+           file_exists: p=>+ fs.existsSync(p),
+           rename: (o, n)=>fs.renameSync(o, n),
+           throw: e=>{throw e;},
+           start_fiber: x=>start_fiber(x),
+           suspend_fiber:
+           wrap_fun
+            ({parameters: ["externref", "funcref", "eqref"],
+              results: ["eqref"]},
+             (f, env)=>new Promise(k=>f(k, env)),
+             {suspending: "first"}),
+           resume_fiber: (k, v)=>k(v),
+           weak_new: v=>new WeakRef(v),
+           weak_deref:
+           w=>{var v = w.deref(); return v == undefined ? null : v;},
+           weak_map_new: ()=>new WeakMap,
+           map_new: ()=>new Map,
+           map_get:
+           (m, x)=>{var v = m.get(x); return v == undefined ? null : v;},
+           map_set: (m, x, v)=>m.set(x, v),
+           map_delete: (m, x)=>m.delete(x),
+           log: x=>console.log("ZZZZZ", x)},
+        string_ops =
+          {test: v=>+ (typeof v === "string"),
+           compare: (s1, s2)=>s1 < s2 ? - 1 : + (s1 > s2),
+           hash: hash_string,
+           decodeStringFromUTF8Array: ()=>"",
+           encodeStringToUTF8Array: ()=>0};
+       const
+        imports =
+          Object.assign
+           ({Math: math,
+             bindings: bindings,
+             js: js,
+             "wasm:js-string": string_ops,
+             "wasm:text-decoder": string_ops,
+             "wasm:text-encoder": string_ops,
+             env: {}},
+            generated),
+          options = {builtins: ["js-string", "text-decoder", "text-encoder"]};
+	  const start_startup = Date.now();	  
+          const wasmModule =
+		isNode
+		? await WebAssembly.instantiate(await code, imports, options)
+		: await WebAssembly.instantiateStreaming(code, imports, options);
+	  const stop_startup = Date.now();
+	  const time_startup = stop_startup - start_startup;
+       var
+        {caml_callback: caml_callback,
+          caml_alloc_tm: caml_alloc_tm,
+          caml_start_fiber: caml_start_fiber,
+          caml_handle_uncaught_exception: caml_handle_uncaught_exception,
+          caml_buffer: caml_buffer,
+          caml_extract_string: caml_extract_string,
+          _initialize: _initialize}
+         = wasmModule.instance.exports,
+        buffer = caml_buffer?.buffer,
+        out_buffer = buffer && new Uint8Array(buffer, 0, buffer.length);
+       start_fiber =
+        wrap_fun
+         ({parameters: ["eqref"], results: ["externref"]},
+          caml_start_fiber,
+          {promising: "first"});
+       var
+        _initialize =
+          wrap_fun
+           ({parameters: [], results: ["externref"]},
+            _initialize,
+            {promising: "first"}),
+        process = globalThis.process;
+       if(process && process.on)
+        process.on
+         ("uncaughtException",
+          (err, origin)=>caml_handle_uncaught_exception(err));
+       else if(globalThis.addEventListener)
+        globalThis.addEventListener
+         ("error",
+          event=>event.error && caml_handle_uncaught_exception(event.error));
+    const start_main = Date.now();
+    await _initialize();
+    const stop_main = Date.now();
+    const time_main = stop_main - start_main;
+    console.log(`Benchmark wasm_of_ocaml (${path}): {{"time_startup": "${time_startup}", "time_main": "${time_main}", "time_pp": ${0}, "program": "${program}"}} ms.`);
+      })
+  (function(globalThis){"use strict"; return {};}(globalThis))
+ ({"generated":
+   (globalThis=>{
+      var
+       joo_global_object = globalThis,
+       jsoo_exports = globalThis?.module?.export || globalThis;
+      return {"strings":
+              ["Undefined_recursive_module",
+               "Assert_failure",
+               "Sys_blocked_io",
+               "Stack_overflow",
+               "Match_failure",
+               "Not_found",
+               "Division_by_zero",
+               "End_of_file",
+               "Invalid_argument",
+               "Failure",
+               "Sys_error",
+               "Out_of_memory"]};})
+    (globalThis),
+   "src": `${path}${program}.wasm`});
